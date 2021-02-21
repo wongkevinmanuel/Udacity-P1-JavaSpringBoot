f7036fe36390f3be7ce83fa9819ea473dc88824b
kevin <onofrekevin@gmail.com>
2021-02-20 23:03:35 -0500

    Implementacion de project lombok. Se utilizo las anotaciones de lombok (@Getter,@Setter,@NoArgsCtructor) en la clase Notes.java.

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/modelo/Notes.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/modelo/Notes.java
@@ -1,49 +1,62 @@
 package com.udacity.jwdnd.course1.cloudstorage.modelo;
 
-public class Notes {
-    public Integer getNoteId() {
-        return noteId;
-    }
+import lombok.*;
 
-    public void setNoteId(Integer noteId) {
-        this.noteId = noteId;
-    }
+import java.io.Serializable;
 
-    public String getNoteTitle() {
-        return noteTitle;
-    }
-
-    public void setNoteTitle(String noteTitle) {
-        this.noteTitle = noteTitle;
-    }
-
-    public String getNoteDescription() {
-        return noteDescription;
-    }
-
-    public void setNoteDescription(String noteDescription) {
-        this.noteDescription = noteDescription;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Notes implements Serializable {
 
     private Integer noteId;
     private String noteTitle;
     private String noteDescription;
     private Integer userId;
 
-    public Notes(Integer noteId, String noteTitle
-            , String noteDescription, Integer userId) {
-        this.noteId = noteId;
-        this.noteTitle = noteTitle;
-        this.noteDescription = noteDescription;
-        this.userId = userId;
-    }
-    public Notes(){}
+//    public Notes(){
+//
+//    }
+//
+//    public Notes(Integer noteId, String noteTitle
+//            , String noteDescription, Integer userId) {
+//        this.noteId = noteId;
+//        this.noteTitle = noteTitle;
+//        this.noteDescription = noteDescription;
+//        this.userId = userId;
+//    }
+//
+//    public Integer getNoteId() {
+//        return noteId;
+//    }
+//
+//    public void setNoteId(Integer noteId) {
+//        this.noteId = noteId;
+//    }
+//
+//    public String getNoteTitle() {
+//        return noteTitle;
+//    }
+//
+//    public void setNoteTitle(String noteTitle) {
+//        this.noteTitle = noteTitle;
+//    }
+//
+//    public String getNoteDescription() {
+//        return noteDescription;
+//    }
+//
+//    public void setNoteDescription(String noteDescription) {
+//        this.noteDescription = noteDescription;
+//    }
+//
+//    public Integer getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(Integer userId) {
+//        this.userId = userId;
+//    }
+
+//    public Notes(){}
 }
